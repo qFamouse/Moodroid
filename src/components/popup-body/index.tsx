5d3a9a232b9192a40e4f168078be5ffb8b8474f0
Famouse <famouse_ru@mail.ru>
2022-09-14 18:38:33 +0300

    refactor: modify status-bar

--- src/components/popup-body/index.tsx
+++ src/components/popup-body/index.tsx
@@ -1,51 +1,29 @@
 import styles from "./popup-body.module.css";
-import { useCallback, useState } from "react";
-import { readDatabase, readDatabasePromise } from "~utils/readDatabase";
-import { StatusBar } from "~components/statusbar";
+import { useState } from "react";
 import ToggleButton from "~components/toggle-button";
+import StatusBar from "~components/status-bar";
 
 export function PopupBody() {
   const [isCheats, setIsCheats] = useState(true);
-  const [statusMessage, setStatusMessage] = useState("");
-
-  const setStatusMessageHandler = useCallback((text: string) => {
-    setStatusMessage(text);
-  }, []);
-
-  const readFileHandler = useCallback(async (event) => {
-    console.log(event.target.files);
-    setStatusMessageHandler("Loading Database...");
-    try {
-      let text = await readDatabasePromise(event.target.files[0])
-      setStatusMessageHandler("Database readed");
-      console.log(text);
-    }
-    catch(error) {
-      setStatusMessageHandler("Cannot read database");
-      console.log(error);
-    }
-  }, [statusMessage]);
+  const [status, setStatus] = useState("Hello :)");
 
   return (
     <div className={styles.popupBody}>
+        <div className={styles.optionHolder}>
+            <ToggleButton
+                text={"Cheats"}
+                checked={isCheats}
+                onClick={(event) => setIsCheats(event.target.checked)}/>
+        </div>
+        <label className={styles.button}>
+            <input hidden type="file" />
+            Load</label>
+        <label className={styles.button}>
+            <input hidden type="button" />
+            Export
+        </label>
 
-      <div className={styles.optionHolder}>
-        <ToggleButton
-            text={"Cheats"}
-            checked={isCheats}
-            onClick={(event) => setIsCheats(event.target.checked)}/>
-      </div>
-
-      <label className={styles.button}>
-        Load Database
-        <input id="fileUpload" type="file" hidden onInput={readFileHandler}/>
-      </label>
-      <label className={styles.button}>
-        Export Database
-        <input type="button" hidden/>
-      </label>
-      
-      <StatusBar message={statusMessage || "Nothing happening"}/>
+        <StatusBar text={status}/>
     </div>  
   )
 
--- src/components/statusbar/index.tsx
+++
@@ -1,6 +0,0 @@
-import styles from './statusbar.module.css';
-
-export function StatusBar({message}) {
-    
-    return <div className={styles.statusbar}>{message}</div>
-}
