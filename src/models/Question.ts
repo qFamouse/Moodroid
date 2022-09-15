ccefaa5b2fb51ee2423f83eed75ada365d7c31a2
Famouse <famouse_ru@mail.ru>
2022-09-15 22:54:08 +0300

    feat: modify Question type. Saving correct and incorrect answers

--- src/models/Question.ts
+++ src/models/Question.ts
@@ -3,5 +3,6 @@ import type {QuestionType} from "~models/QuestionType";
 export declare type Question = {
     text: string,
     type: QuestionType,
-    answers: string[]
+    correctAnswers: string[],
+    incorrectAnswers: string[]
 };
