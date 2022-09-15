7dd351a81ed0ce8d303a4083b82f690fc08e958c
Famouse <famouse_ru@mail.ru>
2022-09-15 22:52:49 +0300

    chore: updating the parser, collecting incorrect and correct answers

---
+++ src/models/AnswerElement.ts
@@ -0,0 +1,5 @@
+export declare type AnswerElement = {
+    parent: HTMLElement,
+    text: HTMLElement,
+    input: HTMLInputElement,
+};
--- src/utils/QuizParser.ts
+++ src/utils/QuizParser.ts
@@ -3,7 +3,52 @@ import type {AnswerElement} from "~models/AnswerElement";
 import {QuestionType} from "~models/QuestionType";
 
 export class QuizParser {
-    private static hasRows(que : Element) : boolean {
+    public static getQuestionAnswersAsElements(que : HTMLElement) : Array<AnswerElement> {
+        let array = new Array<AnswerElement>();
+
+        switch (this.getQuestionType(que.classList)) {
+            case QuestionType.multichoice:
+                this.getQuestionRows(que).forEach(row => {
+                    array.push({
+                        parent: row as HTMLElement,
+                        text: row.querySelector('[data-region^=answer-label]').lastChild as HTMLElement,
+                        input: row.querySelector("input[type=checkbox], input[type=radio]") as HTMLInputElement
+                    })
+                })
+                break;
+
+            case QuestionType.shortanswer:
+            case QuestionType.essay:
+                let parent = que.querySelector(".answer") as HTMLElement;
+                parent.querySelectorAll(".answer>input,.answer>textarea").forEach(input => {
+                    array.push({
+                        parent: parent,
+                        text: input as HTMLInputElement,
+                        input: input as HTMLInputElement
+
+                    })
+                })
+                break;
+
+            case QuestionType.match:
+                this.getQuestionRows(que).forEach(row => {
+                    array.push({
+                        parent: row as HTMLElement,
+                        text: row.querySelector(".text"),
+                        input: row.querySelector(".control")
+
+                    })
+                });
+                break;
+
+            default:
+                throw new Error("Unsupported type");
+        }
+
+        return array;
+    }
+
+    public static hasRows(que : HTMLElement) : boolean {
         switch (this.getQuestionType(que.classList)) {
             case QuestionType.multichoice:
             case QuestionType.match:
@@ -16,8 +61,30 @@ export class QuizParser {
         }
     }
 
-    private static hasInput(que : Element) : boolean {
+    public static hasInput(que : HTMLElement) : boolean {
         return !this.hasRows(que);
+    }
+
+    public static isCorrectQuestion(que : HTMLElement) : boolean {
+        if (!que.classList.contains(`correct`)) {
+            let grade = que.querySelector('.grade')
+            let parsedGrades = [...grade.textContent.matchAll(/\d[.,]\d+|\d+/g)];
+
+            if (parsedGrades.length == 2) {
+                if (parseFloat(parsedGrades[0][0]) != parseFloat(parsedGrades[1][0])) {
+                    return false;
+                }
+            }
+            else {
+                throw new Error("The score parsing failed. There should be 2 grades")
+            }
+        }
+
+        return true;
+    }
+
+    public static getQuestionImages(que : HTMLElement) : NodeListOf<HTMLImageElement> {
+        return que.querySelectorAll(".qtext img")
     }
 
     public static getQuestionType(domTokenList : DOMTokenList) : QuestionType {
@@ -30,11 +97,11 @@ export class QuizParser {
         throw new Error("Unknown question type")
     }
 
-    public static getQuestionText(que : Element) : string {
+    public static getQuestionText(que : HTMLElement) : string {
         return que.querySelector('.qtext').textContent;
     }
 
-    public static getQuestionRows(que : Element) : NodeListOf<Element> {
+    public static getQuestionRows(que : HTMLElement) : NodeListOf<HTMLElement> {
         if (this.hasRows(que)) {
             return que.querySelectorAll(".answer>[class^=r], tbody>[class^=r]");
         }
@@ -43,48 +110,125 @@ export class QuizParser {
         }
     }
 
-    public static getMultichoiceElements(que : Element) : Array<HTMLElement> {
-        if (QuizParser.getQuestionType(que.classList) == QuestionType.multichoice) {
-            let multichoiceElements = new Array<HTMLElement>()
-            this.getQuestionRows(que).forEach(row => {
-                multichoiceElements.push(row.querySelector('[data-region^=answer-label]').lastChild as HTMLElement);
-            })
-            return multichoiceElements;
-        }
+    public static getCorrectAnswers(que : HTMLElement) : string[] {
+        let correctAnswers : string[] = [];
+        let answers = this.getQuestionAnswersAsElements(que);
 
-        throw new Error("Unsupported type");
-    }
+        if (this.isCorrectQuestion(que)) {
+            // It is assumed that the question is answered correctly and any marked/selected/written answer is correct.
+            switch (this.getQuestionType(que.classList)) {
+                case QuestionType.multichoice:
+                    answers.forEach(answer => {
+                        if (answer.input.checked) {
+                            correctAnswers.push(answer.text.textContent)
+                        }
+                    })
+                    break;
 
-    public static getQuestionTextFromRow(row : Element) {
-        if (new RegExp("\\br\\d+\\b").test(row.classList.value)) {
-            return row.querySelector('[data-region^=answer-label],.text').lastChild.textContent
-        }
-    }
+                case QuestionType.shortanswer:
+                case QuestionType.essay:
+                    answers.forEach(answer => {
+                        correctAnswers.push(answer.input.value);
+                    })
+                    break;
 
-    public static getMatchElements(que : Element) : Array<MatchElement> {
-        if (QuizParser.getQuestionType(que.classList) == QuestionType.match) {
-            let matchElements = new Array<MatchElement>()
-            this.getQuestionRows(que).forEach(row => {
-                let matchQuestion = new MatchElement(
-                    row.querySelector(".text"),
-                    row.querySelector(".control")
-                );
-
-                matchElements.push(matchQuestion);
-            });
-
-            return matchElements;
-        }
-
-        throw new Error("Unsupported type");
-    }
-
-    public static getQuestionInput(que : Element) : HTMLInputElement {
-        if (this.hasInput(que)) {
-            return que.querySelector(".answer>input,.answer>textarea") as HTMLInputElement;
+                case QuestionType.match:
+                    answers.forEach(answer => {
+                        correctAnswers.push(answer.text.textContent); // text
+                        correctAnswers.push(answer.input.querySelector("option:checked").textContent); // control
+                    })
+                    break;
+            }
         }
         else {
-            throw new Error("This type cannot have input element");
+            switch (this.getQuestionType(que.classList)) {
+                case QuestionType.multichoice:
+                    answers.forEach(answer => {
+                        if (this.isCorrect(answer.parent)) {
+                            correctAnswers.push(answer.text.textContent)
+                        }
+                    })
+                    break;
+
+                case QuestionType.match:
+                    answers.forEach(answer => {
+                        if (this.isCorrect(answer.input)) {
+                            correctAnswers.push(answer.text.textContent); // text
+                            correctAnswers.push(answer.input.querySelector("option:checked").textContent); // option
+                        }
+                    })
+                    break;
+            }
         }
+
+        return correctAnswers;
+    }
+
+    public static getIncorrectAnswers(que : HTMLElement) : string[] {
+        let incorrectAnswers : string[] = [];
+        let answers = this.getQuestionAnswersAsElements(que);
+
+        if (this.isCorrectQuestion(que)) {
+            switch (this.getQuestionType(que.classList)) {
+                case QuestionType.multichoice:
+                    answers.forEach(answer => {
+                        if (!answer.input.checked) {
+                            incorrectAnswers.push(answer.text.textContent)
+                        }
+                    })
+                    break;
+
+                case QuestionType.match:
+                    answers.forEach(answer => {
+                        answer.input.querySelectorAll("option:not(:checked)").forEach(option => {
+                            incorrectAnswers.push(answer.text.textContent); // text
+                            incorrectAnswers.push(option.textContent); // option
+                        })
+                    })
+                    break;
+            }
+        }
+        else {
+            switch (this.getQuestionType(que.classList)) {
+                case QuestionType.multichoice:
+                    answers.forEach(answer => {
+                        if (this.isIncorrect(answer.parent)) {
+                            if (answer.input.checked) {
+                                incorrectAnswers.push(answer.text.textContent)
+                            }
+                        }
+                    })
+                    break;
+
+                case QuestionType.match:
+                    answers.forEach(answer => {
+                        if (this.isIncorrect(answer.input)) {
+                            incorrectAnswers.push(answer.text.textContent); // text
+                            incorrectAnswers.push(answer.input.querySelector("option:checked").textContent); // option
+                        }
+                    })
+                    break;
+            }
+        }
+
+        return incorrectAnswers;
+    }
+
+    public static isIncorrect(element : HTMLElement) : boolean {
+        if (element.classList.contains("incorrect") ||
+            element.querySelector(".icon.fa.fa-remove.text-danger.fa-fw")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static isCorrect(element : HTMLElement) : boolean {
+        if (element.classList.contains("correct") ||
+            element.querySelector(".icon.fa.fa-check.text-success.fa-fw")) {
+            return true;
+        }
+
+        return false;
     }
 }
