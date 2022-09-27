86e17363434b07bef082c418b30d8962b73d0625
tinmn9088 <tinman.8.9@yandex.ru>
2022-09-27 18:15:53 +0300

    style: change handlers argument types

--- src/utils/handlers.ts
+++ src/utils/handlers.ts
@@ -6,21 +6,18 @@ import { replacer, reviver } from "./QuestionDatabase";
 import { importQuestionsToLocalStorage, removeAllQuestionsFromLocalStorate, retrieveAllQuestionsFromLocalStorage, retrieveQuestionFromLocalStorage, retrieveQuestionsCountFromLocalStorage, saveQuestionToLocalStorage } from "./storage";
 import { SuccessResponseWithData } from "~models/responses/SuccessResponseWithData";
 import type { ImportRequest } from "~models/requests/ImportRequest";
-import type { ExportRequest } from "~models/requests/ExportRequest";
 import type { AddRequest } from "~models/requests/AddRequest";
 import type { GetRequest } from "~models/requests/GetRequest";
-import type { SizeRequest } from "~models/requests/SizeRequest";
-import type { ClearRequest } from "~models/requests/ClearRequest";
 
 /**
  * Handle request: {command: Command.Import, data: "..."}
  */
-export const handleImport = function(request: ImportRequest, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) {
+export const handleImport = function(request: any, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) {
   if (request.command === Command.Import) {  
     try {
       console.log("Import requested.");
 
-      let newQuestions: Map<string, Question> = JSON.parse(request.data, reviver);
+      let newQuestions: Map<string, Question> = JSON.parse((request as ImportRequest).data, reviver);
       importQuestionsToLocalStorage(newQuestions).then(function(importStatus) {
         sendResponse(new SuccessResponseWithData(importStatus));
         console.log(`Imported ${newQuestions.size - importStatus.failed}`, importStatus);
@@ -36,7 +33,7 @@ export const handleImport = function(request: ImportRequest, sender: chrome.runt
 /**
  * Handle request: {command: Command.Export}
  */
-export const handleExport = function(request: ExportRequest, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) {
+export const handleExport = function(request: any, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) {
   if (request.command === Command.Export) {
     retrieveAllQuestionsFromLocalStorage().then((questions) => {
       let text = JSON.stringify(questions, replacer);
@@ -50,9 +47,9 @@ export const handleExport = function(request: ExportRequest, sender: chrome.runt
 /**
  * Handle request: {command: Command.Add, key: "...", question: "..."}
  */
-export const handleAdd = function(request: AddRequest, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) {
+export const handleAdd = function(request: any, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) {
   if (request.command === Command.Add) {
-    saveQuestionToLocalStorage(request.key, request.question).then(function() {
+    saveQuestionToLocalStorage((request as AddRequest).key, (request as AddRequest).question).then(function() {
       sendResponse(new SuccessResponse());
       console.log("Question added.", request.question);
     });
@@ -63,9 +60,9 @@ export const handleAdd = function(request: AddRequest, sender: chrome.runtime.Me
 /**
  * Handle request: {command: Command.Get, key: "..."}
  */
-export const handleGet = function(request: GetRequest, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) {
+export const handleGet = function(request: any, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) {
   if (request.command === Command.Get) {
-    retrieveQuestionFromLocalStorage(request.key).then(function(question) {
+    retrieveQuestionFromLocalStorage((request as GetRequest).key).then(function(question) {
       sendResponse(new SuccessResponseWithData(question));
       console.log("Question sent.", question);
     });
@@ -76,7 +73,7 @@ export const handleGet = function(request: GetRequest, sender: chrome.runtime.Me
 /**
  * Handle request: {command: Command.Size}
  */
-export const handleSize = function(request: SizeRequest, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) {
+export const handleSize = function(request: any, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) {
   if (request.command === Command.Size) {
     retrieveQuestionsCountFromLocalStorage().then(function(count) {
       sendResponse(new SuccessResponseWithData(count));
@@ -88,7 +85,7 @@ export const handleSize = function(request: SizeRequest, sender: chrome.runtime.
 /**
  * Handle request: {command: Command.Clear}
  */
-export const handleClear = function(request: ClearRequest, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) {
+export const handleClear = function(request: any, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) {
   if (request.command === Command.Clear) {
     removeAllQuestionsFromLocalStorate().then(function() {
       sendResponse(new SuccessResponse());
