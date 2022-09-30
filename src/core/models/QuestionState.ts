38c975ac949f994452d38276908111788e6b968c
Famouse <famouse_ru@mail.ru>
2022-09-30 16:50:17 +0300

    chore: adding util function to parse question state

---
+++ src/core/models/QuestionState.ts
@@ -0,0 +1,11 @@
+export enum QuestionState {
+    // Question is completed
+    correct = "correct",
+    incorrect = "incorrect",
+    notanswered = "notanswered",
+    partiallycorrect = "partiallycorrect",
+    complete = "complete",
+    // Question is not completed
+    notyetanswered = "notyetanswered",
+    answersaved = "answersaved"
+}
