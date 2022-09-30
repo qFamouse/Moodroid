f53580e30d11724b0c4d50f67d67ff8c11fbecbf
Famouse <famouse_ru@mail.ru>
2022-09-30 16:51:16 +0300

    chore: change Question type to class

--- src/models/Question.ts
+++ src/models/Question.ts
@@ -1,8 +1,10 @@
 import type {QuestionType} from "~models/QuestionType";
+import type {IAnswer} from "~core/interfaces/answer";
 
-export declare type Question = {
-    text: string,
-    type: QuestionType,
-    correctAnswers: string[],
-    incorrectAnswers: string[]
-};
+export class Question {
+    constructor(
+        readonly text: string,
+        readonly type: QuestionType,
+        readonly answer: IAnswer
+    ) { }
+}
