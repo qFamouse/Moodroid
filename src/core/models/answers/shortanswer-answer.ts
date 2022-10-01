78fe5eb4c6689924c59ff4a44650879e32d6ef80
Famouse <famouse_ru@mail.ru>
2022-10-01 15:12:57 +0300

    feat: adding shortanswer parser

---
+++ src/core/models/answers/shortanswer-answer.ts
@@ -0,0 +1,5 @@
+import type {IAnswer} from "~core/interfaces/answer";
+
+export class ShortanswerAnswer implements IAnswer {
+    public answer : string;
+}
--- src/question-parser/answer-parser-factory.ts
+++ src/question-parser/answer-parser-factory.ts
@@ -2,12 +2,14 @@ import {QuestionType} from "~models/QuestionType";
 import type {IAnswerParser} from "~core/interfaces/answer-parser";
 import {MultichoiceParser} from "~question-parser/parsers/multichoice-parser";
 import {MatchParser} from "~question-parser/parsers/match-parser";
+import {ShortanswerParser} from "~question-parser/parsers/shortanswer-parser";
 
 export class AnswerParserFactory {
 
     private static questionTypeAnswerParser: Map<QuestionType, () => IAnswerParser> = new Map<QuestionType, () => IAnswerParser>([
         [QuestionType.multichoice, () => new MultichoiceParser()],
-        [QuestionType.match, () => new MatchParser()]
+        [QuestionType.match, () => new MatchParser()],
+        [QuestionType.shortanswer, () => new ShortanswerParser()]
     ]);
 
     static getAnswerParser(questionType: QuestionType): IAnswerParser | undefined {
