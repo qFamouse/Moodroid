80cbb500b2a666c1a5f88341295c1d3fb85a0fdb
Famouse <famouse_ru@mail.ru>
2022-10-01 10:15:49 +0300

    fix: the saved variable is returned

--- src/question-parser/shared/parse-question-text.ts
+++ src/question-parser/shared/parse-question-text.ts
@@ -2,7 +2,7 @@ export function parseQuestionText(que: HTMLElement) : string {
     let qtext : HTMLElement = que.querySelector(".qtext");
 
     if (qtext) {
-        return que.querySelector('.qtext').textContent;
+        return qtext.textContent;
     }
 
     throw new Error(`Cannot parse text from question. qtext is undefined ${que}`);
