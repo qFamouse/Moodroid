6c4673171db63ccfb266686976e84a68d3e50036
Famouse <famouse_ru@mail.ru>
2022-10-02 18:17:55 +0300

    feat: adding match answerer

--- src/answerers/answerers-factory.ts
+++ src/answerers/answerers-factory.ts
@@ -1,10 +1,12 @@
 import {QuestionType} from "~models/QuestionType";
 import type {IAnswerer} from "~core/interfaces/answerer";
 import {MultichoiceAnswerer} from "~answerers/multichoice-answerer";
+import {MatchAnswerer} from "~answerers/match-answerer";
 
 export class AnswerersFactory {
     private static questionTypeAnswerer: Map<QuestionType, () => IAnswerer> = new Map<QuestionType, () => IAnswerer>([
         [QuestionType.multichoice, () => new MultichoiceAnswerer()],
+        [QuestionType.match, () => new MatchAnswerer()]
     ]);
 
     public static getAnswerer(questionType: QuestionType) : IAnswerer | undefined {
