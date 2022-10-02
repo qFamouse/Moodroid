ee6b9c083f88b03c488e366e245ebdf4b932ec30
Famouse <famouse_ru@mail.ru>
2022-10-02 18:45:00 +0300

    feat: adding shortanswer answerer

--- src/answerers/answerers-factory.ts
+++ src/answerers/answerers-factory.ts
@@ -2,11 +2,13 @@ import {QuestionType} from "~models/QuestionType";
 import type {IAnswerer} from "~core/interfaces/answerer";
 import {MultichoiceAnswerer} from "~answerers/multichoice-answerer";
 import {MatchAnswerer} from "~answerers/match-answerer";
+import {ShortanswerAnswerer} from "~answerers/shortanswer-answerer";
 
 export class AnswerersFactory {
     private static questionTypeAnswerer: Map<QuestionType, () => IAnswerer> = new Map<QuestionType, () => IAnswerer>([
         [QuestionType.multichoice, () => new MultichoiceAnswerer()],
-        [QuestionType.match, () => new MatchAnswerer()]
+        [QuestionType.match, () => new MatchAnswerer()],
+        [QuestionType.shortanswer, () => new ShortanswerAnswerer()]
     ]);
 
     public static getAnswerer(questionType: QuestionType) : IAnswerer | undefined {
