2baedf6498d6bfd3a48dc2a9c0a717ba9054749d
Famouse <famouse_ru@mail.ru>
2022-10-03 17:46:47 +0300

    refactor: moving according architecture

--- src/core/utils/answerers-factory.ts
+++ src/core/utils/answerers-factory.ts
@@ -1,10 +1,10 @@
 import {QuestionType} from "~core/enums/question-type";
 import type {IAnswerer} from "~core/interfaces/answerer";
-import {MultichoiceAnswerer} from "~answerers/multichoice-answerer";
-import {MatchAnswerer} from "~answerers/match-answerer";
-import {ShortanswerAnswerer} from "~answerers/shortanswer-answerer";
-import {EssayAnswerer} from "~answerers/essay-answerer";
-import {MultianswerAnswerer} from "~answerers/multianswer-answerer";
+import {MultichoiceAnswerer} from "~core/answerers/multichoice-answerer";
+import {MatchAnswerer} from "~core/answerers/match-answerer";
+import {ShortanswerAnswerer} from "~core/answerers/shortanswer-answerer";
+import {EssayAnswerer} from "~core/answerers/essay-answerer";
+import {MultianswerAnswerer} from "~core/answerers/multianswer-answerer";
 
 export class AnswerersFactory {
     private static questionTypeAnswerer: Map<QuestionType, () => IAnswerer> = new Map<QuestionType, () => IAnswerer>([
