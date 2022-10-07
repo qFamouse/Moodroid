c4a27a6070df0f9f6b7e6fb48d0e4cae56a09561
Famouse <famouse_ru@mail.ru>
2022-10-07 22:11:54 +0300

    chore: adding error handlers in contents scripts

--- src/contents/quiz-attempt.ts
+++ src/contents/quiz-attempt.ts
@@ -11,7 +11,7 @@ export const config: PlasmoContentScript = {
 window.addEventListener("load", async () => {
     let ques = document.querySelectorAll('.que') as NodeListOf<HTMLElement>;
 
-    ques.forEach(que => {
+    ques.forEach((que, i) => {
         let key: string = generateQuestionKey(que);
 
         QuestionDatabase.get(key).then(question => {
@@ -27,111 +27,14 @@ window.addEventListener("load", async () => {
                 return;
             }
 
-            answerer.answer(que, question);
+            try {
+                answerer.answer(que, question);
+            }
+            catch (e) {
+                console.warn(e, i);
+            }
         })
     })
-
-
-    // if (await isVerifiedUser()) {
-    //
-    //     let status = CreateStatus();
-    //
-    //     let ques = document.querySelectorAll('.que') as NodeListOf<HTMLElement>;
-    //
-    //     ques.forEach(que => {
-    //         let squeezedQueText = squeezeText(QuizParser.getQuestionText(que));
-    //
-    //         QuestionDatabase.get(squeezedQueText).then(question => {
-    //             if (question != undefined) {
-    //                 switch (QuizParser.getQuestionType(que.classList)) {
-    //                     case QuestionType.multichoice:
-    //                         QuizParser.getQuestionAnswersAsElements(que).forEach(answer => {
-    //                             question.correctAnswers.forEach(correctAnswer => {
-    //                                 if (answer.text.textContent == correctAnswer) {
-    //                                     // TODO: Tip for multichoice
-    //
-    //                                     // answer.text.addEventListener('mouseover', (event) => {
-    //                                     //     status.style.display = 'block';
-    //                                     // });
-    //                                     // answer.text.addEventListener('mouseout', (event) => {
-    //                                     //     status.style.display = 'none';
-    //                                     // });
-    //
-    //                                     // (answer.parent.querySelector("input[type=checkbox], input[type=radio]") as HTMLInputElement).checked = true;
-    //
-    //                                     if (question.correctAnswers.indexOf(answer.text.textContent) >= 0) {
-    //                                         answer.text.style.background = 'green';
-    //                                     }
-    //                                     else if (question.incorrectAnswers.indexOf(answer.text.textContent) >= 0) {
-    //                                         answer.text.style.background = 'red';
-    //                                     }
-    //                                 }
-    //                             })
-    //                         })
-    //                         break;
-    //
-    //                     case QuestionType.shortanswer:
-    //                     case QuestionType.essay:
-    //                         let inputs = QuizParser.getQuestionAnswersAsElements(que);
-    //                         if (question.correctAnswers.length == 1 && inputs.length == 1) {
-    //                             // TODO: Tip for written answer
-    //                             console.log(inputs[0].input);
-    //                             console.log(question.correctAnswers[0]);
-    //
-    //                             // fillInput(inputs[0].input, question.correctAnswers[0]);
-    //
-    //                             inputs[0].input.value = question.correctAnswers[0];
-    //
-    //                         } else if (question.correctAnswers.length == 0) {
-    //                             console.log("There is no answer to this question in the database")
-    //                         } else {
-    //                             throw new Error(`There is more than one answer for one input.`)
-    //
-    //                         }
-    //                         break;
-    //
-    //                     case QuestionType.match:
-    //                         QuizParser.getQuestionAnswersAsElements(que).forEach(answer => {
-    //                             let correctAnswerIndex = question.correctAnswers.indexOf(answer.text.textContent);
-    //
-    //                             if (correctAnswerIndex >= 0) {
-    //                                 answer.input.querySelectorAll("option").forEach(option => {
-    //                                     // TODO: more cycles, need to optimize
-    //                                     if (option.textContent == question.correctAnswers[correctAnswerIndex + 1]) {
-    //
-    //                                         option.style.background = 'green'
-    //
-    //                                         // option.style.fontStyle = 'italic'
-    //
-    //                                         // option.selected = true;
-    //                                     }
-    //                                 })
-    //                             }
-    //                         })
-    //
-    //                         break;
-    //
-    //                     default:
-    //                         throw new Error("Unsupported type")
-    //                 }
-    //             } else {
-    //                 console.log("There are no matches on the question")
-    //             }
-    //         })
-    //     })
-    // }
 })
-
-function CreateStatus() {
-    let status = document.createElement("div");
-    status.innerHTML = "•";
-    status.style.position = 'fixed';
-    status.style.fontSize = "20px";
-    status.style.bottom = '20px'
-    status.style.left = '20px'
-    status.style.display = 'none'
-    document.body.appendChild(status);
-    return status;
-}
 
 
--- src/contents/quiz-review.ts
+++ src/contents/quiz-review.ts
@@ -9,98 +9,22 @@ export const config: PlasmoContentScript = {
 }
 
 window.addEventListener("load", async () => {
-
-
     let ques = document.querySelectorAll('.que') as NodeListOf<HTMLElement>;
-    ques.forEach(que => {
-        let question: Question = QuestionParser.parse(que);
+    ques.forEach((que, i) => {
 
-        if (!question) {
-            console.log("Can't parse");
-        }
+        try {
+            let question : Question = QuestionParser.parse(que);
 
-        console.log('Correct question', question);
+            if (!question) {
+                console.log("Can't parse");
+                return;
+            }
 
-        let key: string = generateQuestionKey(que);
-        if (key && question) {
+            let key = generateQuestionKey(que);
             QuestionDatabase.add(key, question);
         }
-
+        catch (e) {
+            console.warn(e, i+1)
+        }
     })
-
-
-    // if (await isVerifiedUser()) {
-    //     let ques = document.querySelectorAll('.que') as NodeListOf<HTMLElement>
-    //
-    //     if (ques.length != 0) {
-    //         ques.forEach(que => {
-    //             let question : Question;
-    //             if (QuizParser.isCorrectQuestion(que)) {
-    //                 console.log('Correct question');
-    //             }
-    //             else {
-    //
-    //                 // TODO: check feedback to the right answer
-    //                 console.log('Incorrect question');
-    //             }
-    //
-    //             question = {
-    //                 text: QuizParser.getQuestionText(que),
-    //                 type: QuizParser.getQuestionType(que.classList),
-    //                 correctAnswers: QuizParser.getCorrectAnswers(que),
-    //                 incorrectAnswers: QuizParser.getIncorrectAnswers(que)
-    //             }
-    //
-    //             console.log(question);
-    //
-    //             QuestionDatabase.add(
-    //                 QuestionDatabase.generateKey(question.text, QuizParser.getQuestionImages(que)),
-    //                 question);
-    //         })
-    //     }
-    //     else {
-    //         console.log(`Couldn't find the right questions`)
-    //     }
-    // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // ques.forEach(que => {
-    //     // let parser : NewQuizParser = new NewQuizParser(que as HTMLElement);
-    //
-    //     let a = new NewQuizParser(que as HTMLElement);
-    //
-    //     console.log(QuizParser.getCorrectAnswers(que as HTMLElement));
-    //
-    //     // console.log(QuizParser.getQuestionAnswersAsElements(que));
-    //
-    //
-    //
-    //     return;
-    //
-    //     if (isCorrectQuestion(que)) {
-    //         let type = QuizParser.getQuestionType(que.classList);
-    //         let question : Question = {
-    //             text: QuizParser.getQuestionText(que),
-    //             type: type,
-    //             answers: getCorrectAnswers(que, type)
-    //         }
-    //
-    //         console.log(que.querySelectorAll("img"));
-    //
-    //         QuestionDatabase.add(squeezeText(question.text), question);
-    //     }
-    // })
 })
