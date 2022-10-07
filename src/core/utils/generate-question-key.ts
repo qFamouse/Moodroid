3697fbdf53f37bf642020d18233f7436ab6e36a4
Famouse <famouse_ru@mail.ru>
2022-10-07 21:47:58 +0300

    chore: modify generate question key function

--- src/core/utils/generate-question-key.ts
+++ src/core/utils/generate-question-key.ts
@@ -1,18 +1,19 @@
 import {parseQuestionText} from "~core/utils/parse/parse-question-text";
 
 export function generateQuestionKey(que) : string {
-    let text: string = parseQuestionText(que);
+    let key: string = parseQuestionText(que)
+        .replace(/\s+/g, "") // remove spaces " "
+        .toLowerCase();
 
-    // let images: NodeListOf<HTMLImageElement> = QuestionsUtil.getQuestionImages(que);
-    // images.forEach(image => {
-    //     text += image.src;
-    // });
+    let images : Array<HTMLImageElement> = Array.from(que.querySelectorAll("img"));
 
-    // remove whitespaces
-    text = text.replace(/\s+/g, "");
+    if (images.length > 0) {
+        let sources : Array<URL> = images.map(i => new URL(i.src));
+        // key#img:hostname_last2segments
+        let imgKeys : Array<string> = sources.map(s => `#img:${s.hostname}_${s.pathname.split('/').slice(-2).join('/')}`)
 
-    // lowercase
-    text = text.toLowerCase();
+        key += imgKeys.sort().join("");
+    }
 
-    return text;
+    return key;
 }
