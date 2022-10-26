7120137775c5af2c45a80618761bf1a4b355641f
Famouse <famouse_ru@mail.ru>
2022-10-26 23:12:55 +0300

    chore: implement IAnswerer for match-answerer

--- src/core/answerers/match-answerer.ts
+++ src/core/answerers/match-answerer.ts
@@ -2,20 +2,72 @@ import type {IAnswerer} from "~core/interfaces/answerer";
 import type {Question} from "~core/models/question";
 import type {MatchAnswer} from "~core/models/answers/match-answer";
 
+interface IMatchAction {
+    (option : HTMLOptionElement): void
+}
+
 export class MatchAnswerer implements IAnswerer {
-    answer(que: HTMLElement, question: Question) {
-        let answer = question.answer as MatchAnswer;
+    private enumerator(
+        que: HTMLElement,
+        question: Question,
+        correctAction: IMatchAction = undefined,
+        incorrectAction: IMatchAction = undefined
+    ) : void {
+        let matchAnswer = question.answer as MatchAnswer;
         let rows : NodeListOf<HTMLElement> = que.querySelectorAll("tbody>[class^=r]");
 
         rows.forEach(row => {
             let answerText : string = row.querySelector(".text").textContent;
-            let correctAnswer = answer.answers[answerText];
-            if (correctAnswer) {
+            let answer = matchAnswer.answers[answerText];
+            if (answer) {
                 let options : NodeListOf<HTMLOptionElement> = row.querySelectorAll(".control>select>option");
-                let correctOption = Array.from(options).find(option => option.textContent == correctAnswer.correctAnswer);
-                correctOption.selected = true;
+
+                if (correctAction) {
+                    let correctOption = Array.from(options).find(option => option.textContent == answer.correctAnswer);
+                    if (correctOption) {
+                        correctAction(correctOption);
+                    }
+                }
+
+                if (incorrectAction) {
+                    answer.incorrectAnswers.forEach(incorrectAnswer => {
+                        let incorrectOption = Array.from(options).find(option => option.textContent == incorrectAnswer);
+                        if (incorrectOption) {
+                            incorrectAction(incorrectOption);
+                        }
+                    })
+                }
             }
         });
+    }
 
+    toAdventure(que: HTMLElement, question: Question) {
+        let correctAction : IMatchAction = (option : HTMLOptionElement) => {
+            option.style.color = "#fff800";
+            option.style.background = "#3aa83a";
+        }
+
+        let incorrectAction : IMatchAction = (option : HTMLOptionElement) => {
+            option.style.color = "#fff800";
+            option.style.background = "#b90000";
+        }
+
+        this.enumerator(que, question, correctAction, incorrectAction)
+    }
+
+    toExam(que: HTMLElement, question: Question) {
+        let correctAction : IMatchAction = (option : HTMLOptionElement) => {
+            option.style.fontStyle = "italic";
+        }
+
+        this.enumerator(que, question, correctAction)
+    }
+
+    toHack(que: HTMLElement, question: Question) {
+        let correctAction : IMatchAction = (option : HTMLOptionElement) => {
+            option.selected = true;
+        }
+
+        this.enumerator(que, question, correctAction)
     }
 }
