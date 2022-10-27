12972a03b1d4482fa5cefd9db6b4e29dba8272c3
Famouse <famouse_ru@mail.ru>
2022-10-27 13:32:25 +0300

    chore: implement IAnswerer for shortanswer-answerer

--- src/core/answerers/shortanswer-answerer.ts
+++ src/core/answerers/shortanswer-answerer.ts
@@ -1,14 +1,39 @@
 import type {IAnswerer} from "~core/interfaces/answerer";
 import type {Question} from "~core/models/question";
 import type {ShortanswerAnswer} from "~core/models/answers/shortanswer-answer";
+import {fillInput} from "~core/utils/fill-input";
+
+interface IShortanswerAction {
+    (inputElement : HTMLInputElement): void
+}
 
 export class ShortanswerAnswerer implements IAnswerer {
-    answer(que: HTMLElement, question: Question) {
-        let answer = question.answer as ShortanswerAnswer;
-
+    private static handler(
+        que: HTMLElement,
+        question : Question,
+        correctAction : IShortanswerAction
+    ) : void {
         let input : HTMLInputElement = que.querySelector(".answer>input");
         if (input) {
-            input.value = answer.answer;
+            correctAction(input);
         }
+    }
+
+    adventure = this.hack;
+
+    exam(que: HTMLElement, question: Question) {
+        let correctAction : IShortanswerAction = (inputElement : HTMLInputElement) => {
+            fillInput(inputElement, (question.answer as ShortanswerAnswer).answer)
+        }
+
+        ShortanswerAnswerer.handler(que, question, correctAction);
+    }
+
+    hack(que: HTMLElement, question: Question) {
+        let correctAction : IShortanswerAction = (inputElement : HTMLInputElement) => {
+            inputElement.value = (question.answer as ShortanswerAnswer).answer;
+        }
+
+        ShortanswerAnswerer.handler(que, question, correctAction);
     }
 }
