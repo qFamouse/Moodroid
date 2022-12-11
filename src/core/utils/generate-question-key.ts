548b24a39723f5264bee591f509c9187b11255ba
Famouse <famouse_ru@mail.ru>
2022-12-11 16:49:15 +0300

    fix: refinement of the key generator due to moodle scripts

--- src/core/utils/generate-question-key.ts
+++ src/core/utils/generate-question-key.ts
@@ -1,27 +1,22 @@
-import { parseQuestionText } from "~core/utils/parse/parse-question-text"
+import { parseQuestionText } from "~core/utils/parse/parse-question-text";
 
-export function generateQuestionKey(que): string {
+export function generateQuestionKey(que : HTMLElement): string {
     let key: string = parseQuestionText(que)
         .replace(/\s+/g, "") // remove spaces " "
-        .toLowerCase()
+        .toLowerCase();
 
-    let images: Array<HTMLImageElement> = Array.from(
-        que.querySelectorAll("img")
-    )
+    // Take pictures from the content
+    // Because moodle scripts convert some images (img) to inputs (input[type=image]).
+    // This was found in 'info'
+    let images: Array<HTMLImageElement> = Array.from(que.querySelectorAll(".content img"));
 
     if (images.length > 0) {
-        let sources: Array<URL> = images.map((i) => new URL(i.src))
+        let sources: Array<URL> = images.map((i) => new URL(i.src));
         // key#img:hostname_last2segments
-        let imgKeys: Array<string> = sources.map(
-            (s) =>
-                `#img:${s.hostname}_${s.pathname
-                    .split("/")
-                    .slice(-2)
-                    .join("/")}`
-        )
+        let imgKeys: Array<string> = sources.map((s) => `#img:${s.hostname}_${s.pathname.split("/").slice(-2).join("/")}`);
 
-        key += imgKeys.sort().join("")
+        key += imgKeys.sort().join("");
     }
 
-    return key
+    return key;
 }
