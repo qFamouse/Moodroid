96d814318f531f0439b57c1102c516e01ea8040e
Famouse <famouse_ru@mail.ru>
2022-12-17 09:58:26 +0300

    chore: collect answers by 'CollectAnswersState'

--- src/contents/quiz-review.ts
+++ src/contents/quiz-review.ts
@@ -5,6 +5,7 @@ import { QuestionParser } from "~core/parsers/question-parser";
 import { AccessValidator } from "~core/utils/access-validator";
 import { generateQuestionKey } from "~core/utils/generate-question-key";
 import { QuestionDatabase } from "~db/question-database";
+import {ExtensionApi} from "~core/utils/extension-api";
 
 export const config: PlasmoContentScript = {
     matches: ["*://newsdo.vsu.by/mod/quiz/review.php*"]
@@ -12,6 +13,10 @@ export const config: PlasmoContentScript = {
 
 window.addEventListener("load", async () => {
     await AccessValidator.validate();
+
+    if (!await ExtensionApi.getCollectAnswersState()) {
+        return;
+    }
 
     let ques = document.querySelectorAll(".que") as NodeListOf<HTMLElement>;
     ques.forEach((que, i) => {
